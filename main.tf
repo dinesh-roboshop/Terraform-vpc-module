8715b314927c31e4d363f76b21d4ef773a0d149b
Dinesh babu <dbramanayanam@gmail.com>
2025-08-26 05:03:17 +0400

    ading vpc module

--- main.tf
+++ main.tf
@@ -2,17 +2,23 @@ resource "aws_vpc" "my-vpc" {
   cidr_block       = var.cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
 
-  tags = merge (var.common_tags, var.vpc_tags, {
-    Name = local.name
-   }
+  tags = merge(
+    var.common_tags, 
+    var.vpc_tags, 
+    {
+     Name = local.name
+    }
   )
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.my-vpc.id
 
-  tags = merge (var.common_tags, var.igw_tags, {
-    Name = local.name 
-   }
+  tags = merge(
+    var.common_tags, 
+    var.igw_tags, 
+    {
+     Name = local.name 
+    }
   )
 }
