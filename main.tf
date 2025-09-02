16c1876c46201be8ce09d47c5fa3d19cff6b5048
Dinesh babu <dbramanayanam@gmail.com>
2025-09-02 05:46:23 +0400

    ading vpc module

--- main.tf
+++ main.tf
@@ -153,3 +153,22 @@ resource "aws_route" "database_route" {
   nat_gateway_id  = aws_nat_gateway.natgw.id
 }
 
+resource "aws_route_table_association" "public_association" {
+  count = length(var.public_subnet_cidr)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_association" {
+  count = length(var.private_subnet_cidr)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+
+resource "aws_route_table_association" "database_association" {
+  count = length(var.database_subnet_cidr)
+  subnet_id      = aws_subnet.database_subnet[count.index].id
+  route_table_id = aws_route_table.database_rt.id
+}
+
