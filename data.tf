9bf5d9e3ad9744f9823900fc6e3a82945d80fe4c
Dinesh babu <dbramanayanam@gmail.com>
2025-09-05 07:14:26 +0400

    ading vpc module

--- data.tf
+++ data.tf
@@ -5,3 +5,18 @@ data "aws_availability_zones" "azs" {
 data "aws_vpc" "default" {
  default = true
 }
+
+data "aws_route_table" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
--- vpc-peering.tf
+++ vpc-peering.tf
@@ -12,3 +12,35 @@ resource "aws_vpc_peering_connection" "vpc-peering" {
     }
   )
 }
+
+resource "aws_route" "acceptor_Rote" {
+  count = var.is_peering_required && var.accepter_vpc_id == ""  ? 1 : 0 
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc-peering[0].id
+
+}
+
+resource "aws_route" "requestor_public_rote" {
+  count = var.is_peering_required && var.accepter_vpc_id == ""  ? 1 : 0 
+  route_table_id            = aws_route_table.public_rt.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block 
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc-peering[0].id
+
+}
+
+resource "aws_route" "requestor_private_rote" {
+  count = var.is_peering_required && var.accepter_vpc_id == ""  ? 1 : 0 
+  route_table_id            = aws_route_table.private_rt.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block 
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc-peering[0].id
+
+}
+
+resource "aws_route" "requestor_database_rote" {
+  count = var.is_peering_required && var.accepter_vpc_id == ""  ? 1 : 0 
+  route_table_id            = aws_route_table.database_rt.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block 
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc-peering[0].id
+
+}
