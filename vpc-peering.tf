f6eec8348b0d953dcc5650bb713f958554a2842f
Dinesh babu <dbramanayanam@gmail.com>
2025-09-05 10:58:26 +0400

    ading vpc module

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "vpc-peering" {
   count = var.is_peering_required ? 1 : 0
   peer_vpc_id   = var.accepter_vpc_id == "" ? data.aws_vpc.default.id : var.accepter_vpc_id
-  vpc_id        = aws_vpc.my-vpc.id
+  vpc_id        = aws_vpc.my_vpc.id
   auto_accept   = var.accepter_vpc_id == "" ? true : false
 
   tags = merge(
