9eea7d2f1e203214993a97c7f1c4d9bd0d3e5801
Dinesh babu <dbramanayanam@gmail.com>
2025-09-05 11:27:26 +0400

    ading vpc module

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,16 @@ output azs {
 output "vpc_id" {
   value       = aws_vpc.my_vpc.id
 }
+
+output "public_subnet_id" {
+  value       = aws_subnet.public_subnet[*].id
+
+}
+
+output "private_subnet_id" {
+  value       = aws_subnet.private_subnet[*].id
+}
+
+output "database_subnet_id" {
+  value       = aws_subnet.database_subnet[*].id
+}
